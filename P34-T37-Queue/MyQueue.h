3344cd92e7169a1100f5c4c828fab6d56dfcb86a
lapshuntanya <lapshun.tatiana@gmail.com>
2024-12-05 16:54:48 +0200

    ~Queue()

--- P34-T37-Queue/MyQueue.h
+++ P34-T37-Queue/MyQueue.h
@@ -27,7 +27,9 @@ namespace myQueue {
 		Queue() {
 			head = current = nullptr;
 		}
-		~Queue() {   }//реалізація
+		~Queue() {  
+			while (!isEmpty()) pop_front();		
+		}//реалізація
 
 		bool isEmpty()const { 
 			return head == nullptr;
