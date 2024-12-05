6877eab66bd52d7e86b7c9c0dc0c46abf9c3aa32
lapshuntanya <lapshun.tatiana@gmail.com>
2024-12-05 18:05:49 +0200

    main

--- P34-T37-Queue/P34-T37-Queue.cpp
+++ P34-T37-Queue/P34-T37-Queue.cpp
@@ -1,8 +1,9 @@
 #include "MyQueue.h"
+#include "MyPriorityQueue.h"
 
 int main()
 {
-    myQueue::Queue<int> a;
+   /* myQueue::Queue<int> a;
     a.show();
     a.push_back(7);
     a.push_back(4);
@@ -14,6 +15,21 @@ int main()
         a.show();
         a.pop_front();
     }
-    a.show();
+    a.show();*/
+
+    myQueue::PriorityQueue<int> b;
+    b.push_back(7);
+    b.push_back(3);
+    b.push_back(5);
+    b.push_back(9);
+    b.push_back(1);
+    b.push_back(4);
+
+    while (!b.isEmpty()) {
+        cout << "First: " << b.first() << endl;
+        b.show();
+        b.pop_front();
+    }
+
 }
 
